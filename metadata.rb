c1a0f9e20b1c115242b9f3e4153f4abffa071b32
Robert Coleman <github@robert.net.nz>
2014-06-09 21:53:30 +1200

    Version bump to 0.1.5 (for development)

--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,5 @@
 name 'openvpnas'
-version '0.1.4'
+version '0.1.5'
 description 'OpenVPN Access Server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 maintainer 'Robert Coleman'
