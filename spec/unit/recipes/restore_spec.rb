9103ec014ea3da78eb9c7bb9bf1a45e5c5e242f0
Alex Romanov <alex@romanov.ws>
2016-07-14 14:31:03 -0700

    Add serverspec tests

--- spec/unit/recipes/restore_spec.rb
+++ spec/unit/recipes/restore_spec.rb
@@ -3,4 +3,8 @@ require_relative '../../spec_helper'
 describe 'openvpnas::restore' do
   let(:chef_run) { ChefSpec::Runner.new.converge(described_recipe) }
 
+  it 'converges successfully' do
+    expect { chef_run }.to_not raise_error
+  end
+
 end
